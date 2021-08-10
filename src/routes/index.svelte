94cbe6e75757253ab506c6df738972b2278c70ba
Nils Lockean <nils@oribi.se>
2021-08-10 19:07:35 +0200

    style: relaxed intro text on homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <h1>Dubbel8</h1>
-<p class="text-lg my-4 font-medium">
+<p class="text-xl my-4 font-medium leading-relaxed">
 	Upplev Skåne på höjden med Dubbel8! Vi erbjuder prova på-klättringar vid Snuvestuan för både
 	nybörjare och mer avancerade deltagare. <a href="/prova-pa">Läs mer om vad som ingår</a>
 	eller <a href="/bokning">boka ditt eget klätterpaket</a>. Perfekt för dig som är vertikalt nyfiken
