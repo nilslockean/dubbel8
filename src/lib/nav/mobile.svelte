e93b5059dba14735f70b42b4a3448ac454a9f1b4
Nils Lockean <nils@oribi.se>
2021-08-25 15:24:18 +0200

    style: increase mobile nav padding and consitency

--- src/lib/nav/mobile.svelte
+++ src/lib/nav/mobile.svelte
@@ -5,13 +5,13 @@
 	export let onMouseUp: () => void;
 </script>
 
-<nav class="bg-yellow-100 p-4 flex flex-col divide-y divide-yellow-200 md:hidden">
+<nav class="bg-yellow-100 flex flex-col divide-y divide-yellow-200 px-2 md:hidden">
 	{#each $navigation as { title, path }}
 		<a
 			on:mouseup={onMouseUp}
 			class:active={$page.path === path}
 			href={path}
-			class="whitespace-nowrap font-bold uppercase p-2 items-center">{title}</a
+			class="whitespace-nowrap font-bold uppercase p-4 items-center">{title}</a
 		>
 	{/each}
 </nav>
