f0969ba0a53e7202e2c6eb4bdb558d4e02a2e756
Nils Lockean <nils@oribi.se>
2021-09-07 19:11:58 +0200

    fix: footer social nav icons

--- src/lib/nav/social.svelte
+++ src/lib/nav/social.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import Icon from '../icon/index.svelte';
+	export let fill: string | undefined;
 </script>
 
 <nav class="text-center">
 	<a href="https://instagram.com/dubbel8" title="Följ oss på Instagram">
-		<Icon name="instagram-square" />
+		<Icon name="instagram-square" {fill} />
 	</a>
 	<a href="https://facebook.com/dubbel8" title="Följ oss på Facebook">
-		<Icon name="facebook-square" />
+		<Icon name="facebook-square" {fill} />
 	</a>
 </nav>
 
