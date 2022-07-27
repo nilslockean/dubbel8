6f9d13c5139180918c61c3fd412a8b6380a790cc
Nils Lockean <nilslockean@gmail.com>
2022-07-27 12:18:00 +0200

    Use vite build instead of svelte-kit build

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,26 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    "extensions": [".svelte", ...mdsvexConfig.extensions],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
-    kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+	kit: {
 		adapter: adapter({
 			// default options are shown
 			pages: '.svelte-kit/static/build',
 			assets: '.svelte-kit/static/build',
 			fallback: null
-		})
+		}),
+		prerender: {
+			default: true
+		}
 	}
 };
 
